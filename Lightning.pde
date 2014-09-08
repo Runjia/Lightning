fb367cc5f2695d255d928fe71f279609c4d51583
Runjia <runjia.chen44@gmail.com>
2014-09-08 12:26:59 -0700

    Lightning Program

--- Lightning.pde
+++ Lightning.pde
@@ -1,13 +1,39 @@
+int startX=0;
+int startY=150;
+int endX=0;
+int endY=150;
+
 void setup()
 {
   size(300,300);
+  strokeWeight(5);
+  background(0,0,0);
 }
+
 void draw()
 {
+	int randomStroke1=(int)(Math.random()*256);
+	int randomStroke2=(int)(Math.random()*256);
+	int randomStroke3=(int)(Math.random()*256);
+
+	stroke(randomStroke1, randomStroke2, randomStroke3);
+		
+	while (endX<300){
+		int rNum=(int)(Math.random()*10);
+		int rNum2=(int)(Math.random()*19-9);
+		endX=startX+rNum;
+		endY=startY+rNum2;
+
+		line(startX,startY,endX,endY);
+		startX=endX;
+		startY=endY;	
+	}
 
 }
-void mousePressed()
-{
 
+void mousePressed(){
+			startX=0;
+			startY=150;
+			endX=0;
+			endY=150;
 }
-
--- build-tmp/source/Lightning.java
+++ build-tmp/source/Lightning.java
@@ -14,21 +14,45 @@ import java.io.IOException;
 
 public class Lightning extends PApplet {
 
+int startX=0;
+int startY=150;
+int endX=0;
+int endY=150;
+
 public void setup()
 {
   size(300,300);
-  noLoop();
-}
-public void draw()
-{
-  int x = (int)(Math.random() * 300);
-  ellipse(x,150,10,10);
-}
-public void mousePressed()
-{
-  redraw();
+  strokeWeight(5);
+  background(0,0,0);
 }
 
+public void draw()
+{
+	int randomStroke1=(int)(Math.random()*256);
+	int randomStroke2=(int)(Math.random()*256);
+	int randomStroke3=(int)(Math.random()*256);
+
+	stroke(randomStroke1, randomStroke2, randomStroke3);
+		
+	while (endX<300){
+		int rNum=(int)(Math.random()*10);
+		int rNum2=(int)(Math.random()*19-9);
+		endX=startX+rNum;
+		endY=startY+rNum2;
+
+		line(startX,startY,endX,endY);
+		startX=endX;
+		startY=endY;	
+	}
+
+}
+
+public void mousePressed(){
+			startX=0;
+			startY=150;
+			endX=0;
+			endY=150;
+}
   static public void main(String[] passedArgs) {
     String[] appletArgs = new String[] { "Lightning" };
     if (passedArgs != null) {
